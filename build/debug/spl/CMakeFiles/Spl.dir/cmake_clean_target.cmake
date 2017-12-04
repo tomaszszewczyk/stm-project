fecf35a1a068c4106e2a69d01981c570402eb059
Tomasz <tomasz.szewczyk02@gmail.com>
2017-12-04 21:37:01 +0100

    removed build from repo

--- build/debug/spl/CMakeFiles/Spl.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libSpl_debug.a"
-)
---
+++ build/release/spl/CMakeFiles/Spl.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Spl.dir/src/core_cm3.c.obj"
+  "CMakeFiles/Spl.dir/src/misc.c.obj"
+  "CMakeFiles/Spl.dir/src/startup_stm32f10x_md.s.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_adc.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_bkp.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_can.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_cec.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_crc.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_dac.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_dbgmcu.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_dma.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_exti.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_flash.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_fsmc.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_gpio.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_i2c.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_iwdg.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_pwr.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_rcc.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_rtc.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_sdio.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_spi.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_tim.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_usart.c.obj"
+  "CMakeFiles/Spl.dir/src/stm32f10x_wwdg.c.obj"
+  "CMakeFiles/Spl.dir/src/system_stm32f10x.c.obj"
+  "libSpl.pdb"
+  "libSpl.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/Spl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
